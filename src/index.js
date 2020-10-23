b036585f116feafb68606713b70a304e7bb43899
Arkadiusz Milewski <asmilewski@gmail.com>
2020-10-23 11:22:17 +0200

    comment example

--- src/index.js
+++ src/index.js
@@ -4,9 +4,21 @@ import ReactDOM from 'react-dom';
 
 const App = () => {
   return (
-    <div>
-      "Hi There"
-    
+    <div className="ui container comments">
+      <div className="comment" >
+        <a href= '/' className="avatar">
+          <img alt="avatar" />
+        </a>
+        <div className="content">
+          <a href="author">
+            Sam
+          </a>
+          <div className="metadata">
+            <span className="date"> Today at 06:00PM</span>
+            <div classNames="text"> Nice blog Post!</div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 };

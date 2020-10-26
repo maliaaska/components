a7ea016cbf4a6196348a572ffe8db66d69133b2e
Arkadiusz Milewski <asmilewski@gmail.com>
2020-10-26 17:42:19 +0100

    faker js used to generate random data

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import faker from 'faker';
 
 const App = () => {
   return (
     <div className="ui container comments">
       <div className="comment" >
         <a href= '/' className="avatar">
-          <img alt="avatar" />
+          <img alt="avatar" src={faker.image.avatar()} />
         </a>
         <div className="content">
           <a href="author">
@@ -15,8 +15,8 @@ const App = () => {
           </a>
           <div className="metadata">
             <span className="date"> Today at 06:00PM</span>
-            <div classNames="text"> Nice blog Post!</div>
           </div>
+          <div classNames="text"> Nice blog Post!</div>
         </div>
       </div>
     </div>
